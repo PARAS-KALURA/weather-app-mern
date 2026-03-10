3dfe05407b69cef8c624f0a898755f8d21ae8210
Paras Kalura <officialkalura@gmail.com>
2026-03-10 05:54:42 +0530

    Backend Done

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,13 +3,20 @@ const axios = require("axios");
 const getWeather = async (req, res) => {
   try {
     const city = req.params.city;
+    console.log("City:",city);
+    
     const apiKey = process.env.WEATHER_API_KEY;
 
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     );
 
-    res.json(response.data);
+    res.json({
+       city: response.data.name,
+       temperature: response.data.main.temp,
+       humidity: response.data.main.humidity,
+       weather: response.data.weather[0].description
+    });
 
   } catch (error) {
     console.log(error.response?.data || error.message);
--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {getWeather} = require("../controllers/weatherController")
 
-router.get("/weather/:id", getWeather);
+router.get("/weather/:city", getWeather);
 
 module.exports = router;
